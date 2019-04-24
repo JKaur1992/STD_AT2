92e218ba825b046a0e9ee893c2a566751880ded3
JKaur1992 <48848841+JKaur1992@users.noreply.github.com>
2019-04-24 20:42:10 +1000

    Update Rough Code.R

--- Rough Code.R
+++ Rough Code.R
@@ -2,109 +2,123 @@ library(tidyverse)
 library(readxl)
 library(Amelia)
 
-## these are just extra packages that i don't know if we'll need yet. I just copied them from my DAM Assignments for EDA.
-library('ggplot2') # visualization
-library('ggthemes') # visualization
-library('scales') # visualization
-library('dplyr') # data manipulation
-library('mice') # imputation
-library(gtools) # for discretisation
-library(corrplot)
-library(Hmisc)
-library(devtools)
-library(PerformanceAnalytics)
-library(FactoMineR)
-
 ################################
 ## Load the data
 ################################
 
 postcode_data <- read_csv("7_PostcodeData2018.csv")
 suburbdata <- read_csv("9_SuburbData2018.csv")
-
-Alcohol_Violence <- read_excel("1_Alcohol Related Violence.xls")
-Alcohol_Assualts <- read.csv("3_EDITED - Incidents of Assault (Non-domestic assault) occurring during Weekends Nights from January 2009 to December 2018.csv")
-Domestic_Violence <- read.csv("4_EDITED - Incidents of Assault occurring during Weekends Nights on Residential Premises from January 2009 to December 2018.csv")
-NSWcrimes <- read_excel("5_Incident_by_NSW.xlsm")
-OPT <- read_excel("6_Offences on public transport.xls")
-RCIdata <- read_excel("8_RCI_offencebymonth.xlsm") # R's for some reasons renamed all the variables. how do i go back to original names?
-
-healthdata <- read_excel("2_AlcoholConsumptionHealthNSW.xls")
+alcohol_consumption <- read_csv("beh_alc_lhn_trend.csv")
+alcohol_hospitalisations <- read_csv("beh_alcafhos_lhn_trend.csv")
+alcohol_frequency <- read_csv("beh_alcfreq_lhn_trend.csv")
 
 ################################
 ## Basic Data Read
 ################################
 
-sumamry(postcode_data)
-dim(postcode_data)
-str(postcode_data)
 names(postcode_data)
-
-summary(suburbdata)
-dim(suburbdata)
-str(suburbdata)
 names(suburbdata)
+names(alcohol_hospitalisations)
 
-summary(RCIdata)
-dim(RCIdata)
-str(RCIdata)
-names(RCIdata)
+sumamry(postcode_data)
+summary(suburbdata)
+summary(alcohol_consumption)
+summary(alcohol_hospitalisations)
+summary(alcohol_frequency)
 
-summary(Alcohol_Assualts)
-dim(Alcohol_Assualts)
-str(Alcohol_Assualts)
-names(Alcohol_Assualts)
-
-summary(Domestic_Violence)
-dim(Domestic_Violence)
-str(Domestic_Violence)
-names(Domestic_Violence)
-
-#For Annual Alcohol related Domestic and Non-Domestic Crime data, 
-## NEED TO ADD A COLUMN for violence type - Domestic and Non-Domestic 
-## THEN MERGE the datasets.
-total_alcohol <- rbind(Alcohol_Assualts, Domestic_Violence) ## this is the merging code
-suburb_data <- rbind(RCIdata, suburbdata) ## this isn't working due to renaming of the variables in RCI file. maybe just skip the RCI file?
+str(postcode_data)
+str(suburbdata)
+str(alcohol_consumption)
+str(alcohol_hospitalisations)
+str(alcohol_frequency)
 
 ##################################################
 ## Check for missing values
 ##################################################
 
-missmap(postcode_data, col=c("blue", "red"), legend=FALSE)
-missmap(suburbdata, col=c("blue", "red"), legend=FALSE)
-
-missmap(Alcohol_Assualts, col=c("blue", "red"), legend=FALSE)
-missmap(Domestic_Violence, col=c("blue", "red"), legend=FALSE)
-missmap(RCIdata, col=c("blue", "red"), legend=FALSE)
-
-##OR
-
 missmap(postcode_data, main = "Missing values vs observed")
 missmap(suburbdata, main = "Missing values vs observed")
-
-missmap(Alcohol_Assualts, main = "Missing values vs observed")
-missmap(Domestic_Violence, main = "Missing values vs observed")
-missmap(RCIdata, main = "Missing values vs observed")
+missmap(alcohol_consumption, main = "Missing values vs observed")
+missmap(alcohol_hospitalisations, main = "Missing values vs observed")
+missmap(alcohol_frequency, main = "Missing values vs observed")
 
 ########################################
-##Data engineering
+##Data Cleaning and Prep
 ########################################
 
-# select() allows you to rapidly zoom in on a useful subset using operations based on the names of the variables. 
-select(postcode_data, Postcode, March 2014:Dec 2014) ## The variable need to be re-named so they can be used.
+# Clean the "alcohol hospitalisations" data
+# Remove the NA's /blank data (from all the comments at the end of the csv file)
+alcohol_hospitalisations <- alcohol_hospitalisations %>%
+  filter (year !="") %>%
+  filter ("State comparison" != "Rest of NSW")%>% # Remove the state comparison aggregate from each LHD's data
+  filter ("State comparison" != "Total NSW")
 
-# Create a subset for Variable=X and Variable=Y
-sub = subset(postcode_data, Offence category == "Liquor", select = Postcode:Dec 2018) ##NEED TO SWAP SPACES WITH _
-str(sub)
+# Remove the columns we don't want from alcohol hospitalisations data, and rename the remaining to be more friendly
+names(alcohol_hospitalisations)
+alcohol_hospitalisations <- select (alcohol_hospitalisations, -c("State comparison","LL 95% CI","UL 95% CI"))
+alcohol_hospitalisations <- rename(alcohol_hospitalisations, hospitalisation_num = Number)
+alcohol_hospitalisations <- rename(alcohol_hospitalisations, hospitalisation_rate = "Rate per 100,000 population")
+alcohol_hospitalisations <- rename(alcohol_hospitalisations, LHD = "Local Health Districts")
 
-############################################
-##EDA
-############################################
 
-plot(Alcohol_Assualts$LGA)
+# Clean the alcohol consumption data        
+# Remove NA rows (from comment data at end of .csv files)        
+alcohol_consumption <- alcohol_consumption %>%
+  filter (year !=(is.na (year)))
+#Consumption data is by year... the other two are by financial year... let's forget using consumption for now and use the freq instead
 
-ggplot(Alcohol_Violence, aes(x = Year to December 2015, fill = default)) +
-  geom_bar() +
-  labs(x = '2015') +
-  theme_excel()
+
+names(alcohol_frequency)
+# Clean the alcohol frequency data
+# Remove the NA's /blank data (from all the comments at the end of the csv file)
+alcohol_frequency <- alcohol_frequency %>%
+  filter (year !="") 
+alcohol_frequency <- rename (alcohol_frequency, LHD = "Local Health Districts")
+alcohol_frequency <- rename (alcohol_frequency, freq = "Drinking frequency")
+alcohol_frequency <- rename (alcohol_frequency, estimate = "Actual estimate (Per cent)")
+
+# remove the "All LHDs" aggregate data from local health districts - we have them all can sum them up if we need an all!
+alcohol_frequency <- alcohol_frequency %>%
+  filter (LHD !="All LHDs")
+  
+# remove the colums of data we don't want
+alcohol_frequency <- select (alcohol_frequency, -c("Number of Respondents","LL 95% CI","UL 95% CI"))
+
+# Use "Spread" to create a new variable / column for each "Alcohol Frequency" i.e. never, one per day etc. inpreparation for joining it to the hospitalisation data
+alcohol_frequency <- alcohol_frequency %>%
+  spread(key = freq, value = estimate)
+
+names(alcohol_frequency)
+# rename the new columns to be more user friendly for us
+alcohol_frequency <- rename(alcohol_frequency, freq_daily =  Daily)
+alcohol_frequency <- rename(alcohol_frequency, freq_less_weekly =  "Less than weekly")
+alcohol_frequency <- rename(alcohol_frequency, freq_never =  Never)
+alcohol_frequency <- rename(alcohol_frequency, freq_weekly = Weekly)
+
+names(alcohol_frequency)
+names(alcohol_hospitalisations)
+
+# Years are labelled differently in the two sets - we need to make them the same (i.e. frequency data has "2002-2003"; hospitalisations has "2002-03")
+unique (alcohol_frequency$year)
+unique (alcohol_hospitalisations$year)
+alcohol_hospitalisations <- alcohol_hospitalisations %>%
+  mutate(left_year = substr(alcohol_hospitalisations$year,1,5), right_year = substr(alcohol_hospitalisations$year,6,7))
+
+alcohol_hospitalisations <- alcohol_hospitalisations %>%
+  unite ("year", left_year, right_year, sep = "20", remove = TRUE)
+
+alcohol_hospitalisations
+
+## Now Join the Frequency and Hospitalisations data - join by LHD, year, sex
+
+
+alcohol_freq_hosp <- alcohol_hospitalisations %>%
+  full_join(alcohol_frequency)
+
+summary(alcohol_freq_hosp)
+names(alcohol_freq_hosp)
+
+str(alcohol_freq_hosp)
+str(alcohol_hospitalisations)
+
 
