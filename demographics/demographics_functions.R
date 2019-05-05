28be8ead9fc59ff0dacd6ded1e5e6b48d82abcfc
Cuong Manh <cuongmph@gmail.com>
2019-05-05 06:49:30 +0000

    new demographics data from https://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1410.02012-17?OpenDocument

--- demographics/demographics_functions.R
+++ demographics/demographics_functions.R
@@ -4,15 +4,18 @@ get_LGA_demographics <- function(LGA_name){
   library(tidyverse)
   demographics_data <- read_csv(paste('STD_AT2/demographics/',LGA_name,'.csv', sep=''))
   demographics_data_spread <- (demographics_data %>% spread(Time, Value))
-  return(demographics_data_spread %>% filter(str_detect(MEASURE, '^ERP') | 
-                                               str_detect(MEASURE, '^CAPGAINS') |
-                                               str_detect(MEASURE, '^GIFTS') |
-                                               str_detect(MEASURE, '^HELPS') |
-                                               str_detect(MEASURE, '^INCOME') |
-                                               str_detect(MEASURE, '^INSOLVE') |
-                                               str_detect(MEASURE, '^PENSION')))
+  return(demographics_data_spread)
 }
 
-#Sydney_demographics <- get_LGA_demographics('Sydney')
+Sydney_demographics <- get_LGA_demographics('Sydney')
 
-#View(Sydney_demographics)
+View(Sydney_demographics)
+
+
+return(demographics_data_spread %>% filter(str_detect(MEASURE, '^ERP') | 
+                                             str_detect(MEASURE, '^CAPGAINS') |
+                                             str_detect(MEASURE, '^GIFTS') |
+                                             str_detect(MEASURE, '^HELPS') |
+                                             str_detect(MEASURE, '^INCOME') |
+                                             str_detect(MEASURE, '^INSOLVE') |
+                                             str_detect(MEASURE, '^PENSION')))
