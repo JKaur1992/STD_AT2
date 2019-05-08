e89c93172ede1d2551502d0db2122d412ac0ddb0
JKaur1992 <48848841+JKaur1992@users.noreply.github.com>
2019-05-08 22:28:10 +1000

    changes

--- EDA/EDA - Crime_Pop.R
+++ EDA/EDA - Crime_Pop.R
@@ -13,31 +13,19 @@ offence_data <- read_csv("offence_data.csv")
 colnames(offence_data)
 str(offence_data)
 
-#combine the data from monthly to annual
-offence_data_ag=aggregate(violence_count ~ Year + LGA, data = offence_data, FUN = sum)
-
-#########################################################################################
-#change year from 2-digits to 4-digits
-offence_data$year <- parse_date_time(c('Jan-08':'Dec-18'),orders='my')
-unique(offence_data$year) # this shows that the above code is not working
-
-offence_data$year <- as.Date("Jan-08", format = "%b - %y")
-
-offence_data$year <- strptime(as.character(offence_data$year), "%b-%y")
-offence_data$year <- format(as.Date(offence_data$year, format = "%b-%y"), "%m-%Y")
-
-library(lubridate)
-day <- c("Jan-08":"Dec-18")
-as.Date(parse_date_time(day,"dmy"))
 ##########################################################################################
 
 #split date into 2 columns
 offence_data <- offence_data %>%
-  separate(.,"year",c("Month","Year"),sep="-")
+  separate(.,"year",c("Day","Month","Year"),sep="/")
 
+#combine the data from monthly to annual
+offence_data_ag=aggregate(violence_count ~ Year + LGA, data = offence_data, FUN = sum)
+str(offence_data_ag)
 
 #change year from character to number so both merging datasets have same structure
 offence_data_ag[1:1] = lapply(offence_data_ag[1:1], as.numeric)
+str(offence_data_ag)
 
 ####
 population <- read_csv("Population_Clean.csv")
@@ -47,7 +35,7 @@ colnames(population)
 population_subset = select(population, LGA, Year, Person_Population_Number_Total, Population_Density)
 colnames(population_subset)
 str(population_subset)
-str(offence_data_ag)
+
 
 #merge these datasets
 #mergeCol <- c("LGA", "Year")
