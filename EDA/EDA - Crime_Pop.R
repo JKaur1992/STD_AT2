59afedb60a278c09dc2406426bbc175c73f291e4
Kaur <Jasleen.Kaur@gartner.com>
2019-05-09 13:20:42 +1000

    Update EDA - Crime_Pop.R

--- EDA/EDA - Crime_Pop.R
+++ EDA/EDA - Crime_Pop.R
@@ -9,11 +9,10 @@ library(lubridate)
 library(ggplot2)
 library(ggthemes)
 
+setwd("~/GitHub/STD_AT2/CLEAN DATA")
 offence_data <- read_csv("offence_data.csv")
 colnames(offence_data)
 str(offence_data)
-
-##########################################################################################
 
 #split date into 2 columns
 offence_data <- offence_data %>%
@@ -27,7 +26,22 @@ str(offence_data_ag)
 offence_data_ag[1:1] = lapply(offence_data_ag[1:1], as.numeric)
 str(offence_data_ag)
 
-####
+##EDA for offence data alone
+missmap(offence_data_ag, main = "Missing values vs observed") #nothing missing obviously
+unique(offence_data_ag$LGA)
+mean(offence_data_ag$violence_count)
+median(offence_data_ag$violence_count)
+
+offence_data_ag <- filter(offence_data_ag, LGA == 'Sydney') #filter by Sydney LGA
+
+plot(offence_data_ag$Year)
+plot(offence_data_ag$LGA)
+plot(offence_data_ag$violence_count)
+
+ggplot(data = offence_data_ag) + 
+  geom_line(mapping = aes(x = Year, y = violence_count))
+
+###############NEXT DATASET########################################
 population <- read_csv("Population_Clean.csv")
 colnames(population)
 
@@ -36,8 +50,7 @@ population_subset = select(population, LGA, Year, Person_Population_Number_Total
 colnames(population_subset)
 str(population_subset)
 
-
-#merge these datasets
+#MERGE these datasets
 #mergeCol <- c("LGA", "Year")
 #offencedata <- merge(offence_data_ag, population_subset, by = mergeCol)
 offence_data_EDA <- full_join(offence_data_ag, population_subset, by = c("Year", "LGA"))
@@ -49,10 +62,7 @@ write_csv(offence_data_EDA, path = "offence_data_EDA.csv")
 ##EDA
 ###########################################################
 
-
 missmap(offence_data_EDA, main = "Missing values vs observed") #checking for missing values. The merging didn't work
-
-mean(offence_data_ag$violence_count)
 
 offence_data_ag <- filter(offence_data_ag, LGA == 'Sydney')
 ggplot(data = offence_data_ag) + 
