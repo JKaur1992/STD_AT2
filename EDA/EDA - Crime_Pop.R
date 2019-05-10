05dc5d6f5310fd9f0bb14a8531fbe80053da9f18
Kaur <Jasleen.Kaur@gartner.com>
2019-05-10 13:46:25 +1000

    Update EDA - Crime_Pop.R

--- EDA/EDA - Crime_Pop.R
+++ EDA/EDA - Crime_Pop.R
@@ -18,6 +18,15 @@ str(offence_data)
 offence_data <- offence_data %>%
   separate(.,"year",c("Day","Month","Year"),sep="/")
 
+####################################
+#filter data for 2014, Sydney
+####################################
+offence_data_2014 <- filter(offence_data, ( Year == 2014))
+offence_data_2014 <- filter(offence_data_2014, ( LGA == 'Sydney'))
+offence_data_2014 <- filter(offence_data_2014, ( Postcode == 2000))
+ggplot(data = offence_data_2014) + geom_bar(mapping = aes(x = Month, y = violence_count)) #find the right code
+
+##############################################################
 #combine the data from monthly to annual
 offence_data_ag = aggregate(violence_count ~ Year + LGA, data = offence_data, FUN = sum)
 str(offence_data_ag)
@@ -25,6 +34,7 @@ str(offence_data_ag)
 #change year from character to number so both merging datasets have same structure
 offence_data_ag[1:1] = lapply(offence_data_ag[1:1], as.numeric)
 str(offence_data_ag)
+
 
 #filter for 2012-2017 data
 offence_data_ag <- filter(offence_data_ag, ( Year == 2012 | Year == 2013 | Year == 2014 | Year == 2015 | Year == 2016 | Year == 2017))
@@ -160,7 +170,8 @@ missmap(offence_data_EDA, main = "Missing values vs observed") #data looks good.
 #check the structure of the data 
 str(offence_data_EDA) 
 #not good - change it but keep getting 
-#offence_data_EDA[4:5] = lapply(offence_data_EDA[4:5], as.numeric)
+offence_data_EDA[5:5] = lapply(offence_data_EDA[5:5], as.numeric)
+#need to change 4th column as well
 #str(offence_data_EDA) #sorted
 
 offence_pop <- filter(offence_data_EDA, LGA == 'Sydney')
